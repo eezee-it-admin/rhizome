ea56ecd2dc9b70dff46b7e71ce4ae061ee93a593
Eezee-It <52960194+eezee-it-admin@users.noreply.github.com>
2021-09-08 17:33:07 +0200

    Fix mailjet routes

--- rhizome_website/controllers/main.py
+++ rhizome_website/controllers/main.py
@@ -6,9 +6,9 @@ from odoo import http
 class WebsiteBlog(http.Controller):
 
     @http.route([
-        'cf654f212c11e103eeab302863a0bfa5.txt',
-        'aae183afcc141ccf3312f1203af002a5.txt',
-        'e0f0b0de55adce5b203da8c6021510a0.txt',
+        '/cf654f212c11e103eeab302863a0bfa5.txt',
+        '/aae183afcc141ccf3312f1203af002a5.txt',
+        '/e0f0b0de55adce5b203da8c6021510a0.txt',
     ], type='http', auth="public", website=True)
     def mailjet_pages(self, sitemap=False):
         return ""
